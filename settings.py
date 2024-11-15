ce1598280cd8cafe0ed83c6879e1c2bed85ede29
Vitória Camara <120670231+iamvitoria@users.noreply.github.com>
2024-11-15 14:42:47 -0300

    add settings

---
+++ settings.py
@@ -0,0 +1,6 @@
+DATABASE = {
+    'host': 'iamviitoria.mysql.pythonanywhere-services.com',
+    'user': 'iamviitoria',
+    'password': 'sistema_10',
+    'database': 'iamviitoria$sistema_de_tarefas'
+}
